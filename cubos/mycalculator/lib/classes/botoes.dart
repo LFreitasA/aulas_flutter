bf828c1a07d98d79dc297ee60e3ab6c92876b393
Leonardo Augusto de Freitas <leonardo.freitas@hotmail.com.br>
2023-01-06 23:07:11 +0000

    minha calculadora parte 1

---
+++ cubos/mycalculator/lib/classes/botoes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Botoes {
+  Botoes({required this.cor, required this.numero});
+  String numero;
+  Color cor;
+}
---
+++ cubos/mycalculator/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:mycalculator/classes/app_colors.dart';
+
+import 'calculadora.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: AppColors.backgroundAppBar,
+        ),
+        backgroundColor: AppColors.backgroundApp,
+        body: Calculadora(),
+      ),
+    );
+  }
+}
